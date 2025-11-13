8d90bb923bd2094378c50de4ad8aff4513538a83
Thamaraiselvan S <146072187+Thamarai149@users.noreply.github.com>
2025-11-13 21:12:56 +0530

    addd

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-d9a5ed57'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.svpevournd"
+    "revision": "0.v8f4mlr8abg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
